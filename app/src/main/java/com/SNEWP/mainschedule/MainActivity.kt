907b9eb51324b08b894714bbaf1f1e6cf0a1200f
coldblade2000 <diegogranada7@gmail.com>
2019-10-30 18:25:41 -0500

    started authentication

--- app/src/main/java/com/SNEWP/mainschedule/MainActivity.kt
+++ app/src/main/java/com/SNEWP/mainschedule/MainActivity.kt
@@ -1,41 +1,77 @@
 package com.SNEWP.mainschedule
 
+import android.app.Activity
+import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
-import com.mikepenz.materialdrawer.DrawerBuilder
-
-import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
-
-import android.view.View
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.fragment.app.FragmentActivity
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.ErrorCodes
+import com.firebase.ui.auth.IdpResponse
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
 import com.mikepenz.materialdrawer.AccountHeader
 import com.mikepenz.materialdrawer.AccountHeaderBuilder
 import com.mikepenz.materialdrawer.Drawer
+import com.mikepenz.materialdrawer.DrawerBuilder
 import com.mikepenz.materialdrawer.model.DividerDrawerItem
 import com.mikepenz.materialdrawer.model.PrimaryDrawerItem
 import com.mikepenz.materialdrawer.model.ProfileDrawerItem
 import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem
 import com.mikepenz.materialdrawer.model.interfaces.IProfile
 
-class MainActivity : AppCompatActivity(), ScheduleFragment.OnFragmentInteractionListener {
 
+class MainActivity : AppCompatActivity(), ScheduleFragment.OnFragmentInteractionListener {
+    private val RC_SIGN_IN = 123
     override fun onCreate(savedInstanceState: Bundle?) {
         // https://github.com/mikepenz/MaterialDrawer
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val toolbar = findViewById<Toolbar>(R.id.toolbar)
-            setSupportActionBar(toolbar)
+        setSupportActionBar(toolbar)
+
+        val auth = FirebaseAuth.getInstance()
+
+        var profileName = "Guest"
+        var identity = "0123@gmail.com"
+        val user = FirebaseAuth.getInstance().currentUser
+
+
+        if (user != null) {
+            // already signed in
+            profileName = user.displayName!!
+            if(user.phoneNumber!=null)
+                identity = user.phoneNumber!!
+            if(user.email!=null)
+                identity = user.email!!
+
+
+        } else {
+            // not signed in
+            startActivityForResult(
+                    // Get an instance of AuthUI based on the default app
+                    AuthUI.getInstance()
+                            .createSignInIntentBuilder()
+                            .setAvailableProviders(listOf(
+                                    AuthUI.IdpConfig.GoogleBuilder().build(),
+                                    AuthUI.IdpConfig.EmailBuilder().build(),
+                                    AuthUI.IdpConfig.PhoneBuilder().build()
+                            ))
+                            .build(), RC_SIGN_IN)
+        }
 
         val header = AccountHeaderBuilder()
                 .withActivity(this)
                 .addProfiles(
-                        ProfileDrawerItem().withName("Mike Penz").withEmail("mikepenz@gmail.com")
+                        ProfileDrawerItem().withName(profileName).withEmail(identity)
                 )
                 .withOnAccountHeaderListener(object : AccountHeader.OnAccountHeaderListener {
                     override fun onProfileChanged(view: View?, profile: IProfile<*>, current: Boolean): Boolean {
@@ -46,6 +82,7 @@ class MainActivity : AppCompatActivity(), ScheduleFragment.OnFragmentInteraction
         DrawerBuilder()
                 .withActivity(this)
                 .withToolbar(toolbar)
+                .withAccountHeader(header)
                 .addDrawerItems(PrimaryDrawerItem().withIdentifier(1).withName("Horario"),
                         DividerDrawerItem(),
                         PrimaryDrawerItem().withIdentifier(2).withName("Zonas Escogidas"))
@@ -83,6 +120,36 @@ class MainActivity : AppCompatActivity(), ScheduleFragment.OnFragmentInteraction
         } else super.onOptionsItemSelected(item)
 
     }
+
+    /*override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == RC_SIGN_IN) {
+            val response = IdpResponse.fromResultIntent(data)
+
+            // Successfully signed in
+
+
+            if (resultCode == Activity.RESULT_OK) {
+                /*startActivity(SignedInActivity.createIntent(this, response))
+                finish()*/
+            } else {
+                // Sign in failed
+
+                if (response == null) {
+                    // User pressed back button
+
+                    showSnackbar(R.string.sign_in_cancelled)
+                    return
+                }
+                if (response.error!!.errorCode == ErrorCodes.NO_NETWORK) {
+                    showSnackbar(R.string.no_internet_connection)
+                    return
+                }
+                showSnackbar(R.string.unknown_error)
+                Log.e(FragmentActivity.TAG, "Sign-in error: ", response.error)
+            }
+        }
+    }*/
 
     override fun onFragmentInteraction(uri: Uri) {
 
